c1ef880e10edf56dc440b346549cdf16b241f4f7
Ashika SATHSARANI <ashika.20230077@iit.ac.lk>
2025-04-09 12:00:40 +0530

    index.js

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+import "./styles/globals.css"
+import "./styles/header.css"
+import "./styles/button.css"
+import "./styles/card.css"
+import "./styles/form.css"
+import "./styles/recipe-card.css"
+import "./styles/recipe-details.css"
+import "./styles/badge.css"
+import "./styles/tabs.css"
+import "./styles/search.css"
+import "./styles/timer.css"
+import "./styles/auth.css"
+import App from "./App"
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
