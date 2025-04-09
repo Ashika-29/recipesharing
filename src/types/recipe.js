1e624fba15d72ff5eb565fce1861ca8607b6de9c
Ashika SATHSARANI <ashika.20230077@iit.ac.lk>
2025-04-09 11:45:24 +0530

    Create recipe.js file

---
+++ src/types/recipe.js
@@ -0,0 +1,15 @@
+export default class Recipe {
+    id = ""
+    userId = ""
+    title = ""
+    author = ""
+    image = ""
+    cookingTime = 0
+    servings = 0
+    rating = 0
+    ingredients = []
+    instructions = []
+    dietary = []
+    notes = ""
+    substitutions = []
+  }
